563b9cdf3a671742565713e7cfc98cb616778380
gustavo.carvalho <gustavo.stn@outlook.com.br>
2025-01-27 18:57:54 -0300

    feat: adding Angular Material and Bootstrap for improved UI components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { environment } from '../../../environment/environment.prod';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
---
+++ src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  API: 'https://developer.marvel.com',
+};
