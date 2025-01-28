65dc45e16a7a5c676fdffac7e87069d49a602825
gustavo.carvalho <gustavo.stn@outlook.com.br>
2025-01-28 08:03:09 -0300

    test: improving tests for services

--- src/app/services/v1/characters/characters.service.spec.ts
+++ src/app/services/v1/characters/characters.service.spec.ts
@@ -1,24 +1,87 @@
-import { CharactersService } from './characters.service';
 import { TestBed } from '@angular/core/testing';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { CharactersService } from './characters.service';
+import { RestService } from '../../shared/rest.service';
+import { of, throwError } from 'rxjs';
+import { CharacterApiResponse } from './interface/characters-api.interface';
+import { characterMock } from '../../../../tests/character.mock';
 
-describe('Service: Characters', () => {
+describe('CharactersService', () => {
   let service: CharactersService;
+  let restServiceMock: jest.Mocked<RestService>;
 
   beforeEach(() => {
+    restServiceMock = {
+      get: jest.fn(),
+    } as any;
+
     TestBed.configureTestingModule({
       providers: [
         CharactersService,
-        provideHttpClient(withInterceptorsFromDi()),
+        { provide: RestService, useValue: restServiceMock },
       ],
     });
     service = TestBed.inject(CharactersService);
   });
 
-  it('Should be created', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getCharacters', () => {
+    it('Should handle error response when the API returns an error', (done) => {
+      restServiceMock.get.mockReturnValue(of({ error: true, data: {} }));
+
+      service.getCharacters('0', '10').subscribe((response) => {
+        expect(response.error).toBe(true);
+        done();
+      });
+    });
+  });
+
+  describe('getCharacterByName', () => {
+    it('Should emit ERRORS status when the API response contains an error', (done) => {
+      restServiceMock.get.mockReturnValue(of({ error: true, data: {} }));
+
+      service.onSearchCharactersByName$().subscribe((status) => {
+        if (status.status === 'ERROR') {
+          expect(status.status).toBe('ERROR');
+          done();
+        }
+      });
+
+      service.getCharacterByName('Spiderman').subscribe();
+    });
+  });
+
+  describe('getCharacterByID', () => {
+    it('should return character data when API response is successful', (done) => {
+      const mockCharacter = {
+        id: 1,
+        name: 'Spiderman',
+        thumbnail: {
+          path: 'http://i.annihil.us/u/prod/marvel/i/mg/5/a0/4c0035890fb0a',
+          extension: 'jpg',
+        },
+        firstAppearance: 1962,
+      };
+
+      restServiceMock.get.mockReturnValue(
+        of({ error: false, data: { results: [mockCharacter] } })
+      );
+
+      service.getCharacterByID(1).subscribe((response) => {
+        expect(response.error).toBe(false);
+        expect(response.data.name).toBe('Spiderman');
+        done();
+      });
+    });
+
+    it('Should return error when the API response has an error', (done) => {
+      restServiceMock.get.mockReturnValue(of({ error: true, data: {} }));
+      service.getCharacterByID(1).subscribe((response) => {
+        expect(response.error).toBe(true);
+        done();
+      });
+    });
   });
 });
--- src/app/services/v1/comments/comments.service.spec.ts
+++ src/app/services/v1/comments/comments.service.spec.ts
@@ -1,25 +1,116 @@
 import { TestBed } from '@angular/core/testing';
 import { CommentsService } from './comments.service';
-import { delay, of } from 'rxjs';
+import { RestService } from '../../shared/rest.service';
+import { of } from 'rxjs';
 import { Comment } from './interface/comments-api.interface';
-import { provideHttpClient } from '@angular/common/http';
 
-describe('Service: Comments', () => {
+describe('CommentsService', () => {
   let service: CommentsService;
-  let restServiceMock: any;
+  let restServiceMock: jest.Mocked<RestService>;
+
   beforeEach(() => {
     restServiceMock = {
       post: jest.fn(),
       put: jest.fn(),
       delete: jest.fn(),
-    };
+    } as any;
+
     TestBed.configureTestingModule({
-      providers: [CommentsService, provideHttpClient()],
+      providers: [
+        CommentsService,
+        { provide: RestService, useValue: restServiceMock },
+      ],
     });
     service = TestBed.inject(CommentsService);
   });
 
   it('Should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('createComment', () => {
+    it('SHould successfully create a comment', (done) => {
+      const mockComment: Comment = {
+        id: 1,
+        text: 'Test comment',
+        name: 'Gustavo Carvalho',
+      };
+      restServiceMock.post.mockReturnValue(of({ data: {}, error: false }));
+
+      service.createComment(mockComment).subscribe((result) => {
+        expect(result).toBe(true);
+        done();
+      });
+    });
+
+    it('Should handle error on create comment', (done) => {
+      restServiceMock.post.mockReturnValue(of({ data: {}, error: true }));
+      service
+        .createComment({
+          id: 0,
+          name: 'Gustavo Carvalho',
+          text: 'Test comment',
+        })
+        .subscribe((result) => {
+          expect(result).toBe(true); // it will still return true in the map
+          done();
+        });
+    });
+  });
+
+  describe('editComments', () => {
+    it('Should successfully edit a comment', (done) => {
+      const mockComment: Comment = {
+        name: 'Gustavo Carvalho',
+        id: 3,
+        text: 'Updated comment',
+      };
+
+      restServiceMock.put.mockReturnValue(of({ data: {}, error: false }));
+
+      service.editComment(mockComment).subscribe((result) => {
+        expect(result).toBe(true);
+        done();
+      });
+    });
+
+    it('should handle error on edit comment', (done) => {
+      const mockComment: Comment = {
+        id: 1,
+        text: 'Updated comment',
+        name: 'Gustavo Carvalho',
+      };
+
+      restServiceMock.put.mockReturnValue(of({ data: {}, error: true }));
+
+      service.editComment(mockComment).subscribe((result) => {
+        expect(result).toBe(true); // it will still return true in the map
+        done();
+      });
+    });
+  });
+
+  describe('deleteComment', () => {
+    it('should successfully delete a comment', (done) => {
+      const mockId = 1;
+
+      restServiceMock.delete.mockReturnValue(of({ error: false, data: {} }));
+
+      service.deleteComment(mockId).subscribe((result) => {
+        expect(result).toBe(true);
+        done();
+      });
+    });
+
+    it('should handle error on delete comment', (done) => {
+      const mockId = 1;
+
+      restServiceMock.delete.mockReturnValue(of({ error: true, data: {} }));
+
+      service.deleteComment(mockId).subscribe((result) => {
+        expect(result).toBe(true);
+        done();
+      });
+    });
   });
 });
